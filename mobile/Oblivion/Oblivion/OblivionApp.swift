8eee95892e33f18988478f4e656498eaecaf1303
sreejaveeraghanta <sreeja.veeraghanta@outlook.com>
2025-12-18 22:50:38 -0800

    Add web template

---
+++ mobile/Oblivion/Oblivion/OblivionApp.swift
@@ -0,0 +1,17 @@
+//
+//  OblivionApp.swift
+//  Oblivion
+//
+//  Created by Sreeja V on 2025-12-18.
+//
+
+import SwiftUI
+
+@main
+struct OblivionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
